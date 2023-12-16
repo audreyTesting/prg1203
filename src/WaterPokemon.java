3704c5c7824eef55f746ef154ad180a5afc2b2f0
AudreyBCNS <thisaintaudrey@gmail.com>
2023-12-16 12:09:57 +0800

    changes on 16th Dec 12:09pm

--- src/WaterPokemon.java
+++ src/WaterPokemon.java
@@ -1,2 +1,15 @@
-public class WaterPokemon {
+public class WaterPokemon extends Pokemon{
+
+    //constructors
+    public WaterPokemon(int index, String name, int level, String type, String ability, String species) {
+        super(index, name, level,"Water" ,ability, species);
+    }
+
+
+    //strings
+    @Override
+    public String toString() {
+        return String.format("Index [%d]", super.toString());
+    }
+
 }
